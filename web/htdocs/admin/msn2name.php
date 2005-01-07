67943de2dbdb182158e1d5e6fe49569336c0dff8
jonasge <jonasge@3730feeb-cfd4-0310-94ac-c51b6e4abe79>
2005-01-07 17:53:54 +0000

    admin/msn2name*

--- web/htdocs/admin/msn2name.php
+++ web/htdocs/admin/msn2name.php
@@ -16,29 +16,93 @@
 <?
 include("./check_it.php");
 include("./header.inc.php");
+$zugriff_mysql->connect_mysql($sql["host"],$sql["dbuser"],$sql["dbpasswd"],$sql["db"] );
 ?>
 <div class="ueberschrift_seite">MSN to name</div>
 
 
-Hier kann man den lokaklen MSNs Namen zuordnen, diese Namen tauchen den in der Anrufstatistik anstatt der Nummer auf.<br><br>
+Hier kann man den lokalen MSNs Namen zuordnen, diese Namen tauchen den in der Anrufstatistik anstatt der Nummer auf.<br/><br/>
 
 <?
  if (isset($_GET[loeschen]))
   {
-   $zugriff_mysql->connect_mysql($sql["host"],$sql["dbuser"],$sql["dbpasswd"],$sql["db"] );
-    $result=mysql_query("DELETE FROM msnzuname WHERE id=$_GET[loeschen]");
-	 if ($result==1)
-	   {
-	    echo "<center><br>Eintrag mit ID Nr. $_GET[loeschen] erflogreich gelöscht!<br></center>";
-	   }
-	   else
-	   {
-	    echo "<center><br><font color=\"red\">Eintrag mit ID Nr. $_GET[loeschen] NICHT erflogreich gelöscht!<br></font></center>";
-	   }
-   $zugriff_mysql->close_mysql(); 
+   $result=mysql_query("DELETE FROM msnzuname WHERE id=$_GET[loeschen]");
+    if ($result)
+     {
+      echo "<br/><span style=\"text-align:center;color:blue;\">Database entry with ID $_GET[loeschen] sucessfull deleted</span><br/><br/>";
+     }
+    else
+     {
+      echo "<br/><span style=\"text-align:center;color:red;\">Eintrag mit ID Nr. $_GET[loeschen] NICHT erflogreich gelöscht!</span><br/><br/>";
+     }
+  
   }
 
-  if (isset($_GET[nreintrag]))
+
+
+if (isset($_POST[absenden]))
+ {
+  if (isset($_POST[msn]))
+   {
+     if (isset($_POST[name]))
+	  {
+	  
+	 $result=mysql_query("INSERT INTO msnzuname VALUES('', '$_POST[msn]', '$_POST[name]')");
+		 if ($result)
+		  {
+		   echo "<br/><span style=\"text-align:center;color:blue;\">Daten erflogreich eingetragen!</span><br/><br/>";
+		  }
+		  else
+		  {
+		  echo "<br/><span style=\"text-align:center;color:red;\">Daten NICHT erflogreich eingetragen!</span><br/><br/>>";
+		  }
+	   
+	  }
+	  else
+	  {
+      echo "<br/><span style=\"text-align:center;color:red;\">
+      Please insert name!</span><br/><br/>";
+	  }
+   }
+   else
+   {
+   echo "<br/><span style=\"text-align:center;color:red;\">
+   Please insert MSN</span><br/><br/>";
+   }
+
+ }
+
+
+?>
+
+ <table border="0" style="margin-right:auto;margin-left:auto;">
+   <tr>
+        <td style="width:80px;text-align:center;">MSN</td>
+	<td style="width:80px;text-align:center;">name</td>
+	<td style="width:60px;text-align:center;">edit</td>
+	<td style="width:60px;text-align:center;">delete</td>
+   </tr>
+
+<?
+$result=mysql_query("SELECT * FROM msnzuname");
+ while($row=mysql_fetch_array($result))
+  {
+   echo "<tr>
+    <td>$row[msn]</td>
+	<td>$row[name]</td>
+	<td style=\"text-align:center;\"><a href=\"./msn2nameb.php?bid=$row[id]\"><img src=\"../bilder/edit.png\" style=\"border-width:0px;vertical-align:middle;\" alt=\"\"/></a></td>
+	<td style=\"text-align:center;\"><a href=\"./msn2name.php?loeschen=$row[id]\"><img src=\"../bilder/edittrash.png\" style=\"border-width:0px;vertical-align:middle;\" alt=\"\"/></a></td>
+   </tr>";
+  }
+ 
+?>
+
+
+
+  </table>
+  
+ <? 
+    if (isset($_GET[newentry]))
    {
     echo "<br><center><h2>Neuer Eintrag</h2>";
 	echo "<form action=\"msn2name.php\" method=\"post\">
@@ -56,72 +120,15 @@ Hier kann man den lokaklen MSNs Namen zuordnen, diese Namen tauchen den in der A
 	</form>";
 	echo "<br><br></center>";
    }
+  
+  ?>
+<br/><br/>
+- <a href="./msn2name.php?newentry=yes">New entry</a> -<br>
 
-if (isset($_POST[absenden]))
- {
-  if (isset($_POST[msn]))
-   {
-     if (isset($_POST[name]))
-	  {
-	  $zugriff_mysql->connect_mysql($sql["host"],$sql["dbuser"],$sql["dbpasswd"],$sql["db"] );
-	    $res=mysql_query("INSERT INTO msnzuname VALUES('', '$_POST[msn]', '$_POST[name]')");
-		 if ($res==1)
-		  {
-		   echo "<center><br>Daten erflogreich eingetragen!<br></center>";
-		  }
-		  else
-		  {
-		  echo "<center><font color=\"red\">Daten NICHT erflogreich eingetragen!</font></center>";
-		  }
-	   $zugriff_mysql->close_mysql(); 
-	  }
-	  else
-	  {
-       echo "<center><font color=\"red\">Bitte Name ausfüllen!</font></center>";
-	  }
-   }
-   else
-   {
-   echo "<center><font color=\"red\">Bitte MSN ausfüllen!</font></center>";
-   }
-
- }
-
-
-?>
-<center>
-  <table border="1">
-   <tr>
-    <td>MSN</td>
-	<td>Name</td>
-	<td>Bearbeiten</td>
-	<td>Löschen</td>
-   </tr>
-
-<?
-$zugriff_mysql->connect_mysql($sql["host"],$sql["dbuser"],$sql["dbpasswd"],$sql["db"] );
-$result=mysql_query("SELECT * FROM msnzuname");
- while($row=mysql_fetch_array($result))
-  {
-   echo "<tr>
-    <td>$row[msn]</td>
-	<td>$row[name]</td>
-	<td><center><a href=\"./msn2nameb.php?bid=$row[id]\">OK</a></center></td>
-	<td><center><a href=\"./msn2name.php?loeschen=$row[id]\">OK</a></center></td>
-   </tr>";
-  }
- $zugriff_mysql->close_mysql();
-?>
-
-
-
-  </table>
-<br><br>
-- <a href="./msn2name.php?nreintrag=yes">Neuer Eintrag</a> -<br>
-</center>
 
 
 
 <?
+$zugriff_mysql->close_mysql();
 include("footer.inc.php");
 ?>
--- web/htdocs/admin/msn2nameb.php
+++ web/htdocs/admin/msn2nameb.php
@@ -17,69 +17,66 @@
 include("./check_it.php");
 include("./header.inc.php");
 ?>
-<br>
-<center>
-<h3>MSN zu einem Name zuordnen<br>- Bearbeiten -</h2>
-</center>
-<br>
-
+<div class="ueberschrift_seite">edit MSN to Name</div>
 <?
- if (isset($_POST[speichern]))
-   {
-   echo "ID: $_POST[id]";
+if (isset($_POST[save]))
+ {
+  
   $zugriff_mysql->connect_mysql($sql["host"],$sql["dbuser"],$sql["dbpasswd"],$sql["db"] );
-	$res=mysql_query( "DELETE FROM msnzuname WHERE id=$_POST[id] ");
-	 if ($res==1)
-	  {
-	   echo "<center>Eintrag erfolgreich gelöscht !</center>";
-	    $res1=mysql_query("INSERT INTO msnzuname VALUES('$_POST[id]','$_POST[msn]', '$_POST[name]')");
-		 if ($res1==1)
-		  {
-			echo "<center>Eintrag erfolgreich in Datenbank geschreiben !</center>";
-
-		  }
-		  else
-		  {
-		   echo "<center><font color=\"red\">Eintrag NICHT erfolgreich in Datenbank geschreiben !</font></center>";
-		  }
-	  }
-	  else
-	  {
-	  echo "<center><font color=\"red\">Eintrag NICHT erfolgreich gelöscht</font></center>";
-	  echo mysql_error();
-	  }
-
-	$zugriff_mysql->close_mysql();
+  $res=mysql_query("DELETE FROM msnzuname WHERE id=$_POST[id]");
+  if ($res)
+   {
+    $result=mysql_query("INSERT INTO msnzuname VALUES('$_POST[id]','$_POST[msn]', '$_POST[name]')");
+    if ($result)
+      {
+	echo "<span style=\"text-align:center;color:blue;\">Eintrag erfolgreich in Datenbank geschreiben !</span>";
+      }
+    else
+      {
+      echo "<span style=\"text-align:center;color:red;\">Eintrag NICHT erfolgreich in Datenbank geschreiben !</span>";
+      }
+   }
+   else
+   {
+    echo "<center><font color=\"red\">Eintrag NICHT erfolgreich gelöscht</font></center>";
+    echo mysql_error();
+    }
+$zugriff_mysql->close_mysql();
    }
 ?>
 
-<center>
-<form action="msn2nameb.php" method="post">
-<table border="1">
-<tr>
- <td>MSN</td>
- <td>Name</td>
-</tr>
 <?
+if (isset($_GET[bid]))
+ {
 $zugriff_mysql->connect_mysql($sql["host"],$sql["dbuser"],$sql["dbpasswd"],$sql["db"] );
- $result=mysql_query("SELECT * FROM msnzuname WHERE id='$_GET[bid]'");
- $daten=mysql_fetch_array($result);
-
- echo "<tr>
-<td><input type=\"text\" name=\"msn\"  value=\"$daten[msn]\"></td>
-<td><input type=\"text\" name=\"name\" value=\"$daten[name]\"></td>
-
-
- </tr>";
+$result=mysql_query("SELECT id,name,msn FROM msnzuname WHERE id='$_GET[bid]'");
+$daten=mysql_fetch_array($result);
 $zugriff_mysql->close_mysql();
 ?>
+<form action="msn2nameb.php" method="post">
+<table border="0"  style="margin-right:auto;margin-left:auto;">
+<tr>
+ <td style="text-align:left;">MSN:</td>
+ <td stype="width:7px;"></td>
+ <td style="text-align:right;">
+ <?="<input type=\"text\" name=\"msn\"  value=\"$daten[msn]\"/>"?></td>
+</tr>
+<tr>
+ <td style="text-align:left;">name:</td>
+ <td stype="width:7px;"></td>
+ <td style="text-align:right;">
+ <?="<input type=\"text\" name=\"name\" value=\"$daten[name]\"/>"?></td>
+</tr>
+<tr>
+ <td colspan="3"><br/>
+<input type="hidden" name="id" value="<?="$_GET[bid]"?>"/>
+<input type="submit" name="save" value="save"/></td>
+</tr>
 </table>
-<br>
-<input type="hidden" name="id" value="<? echo "$_GET[bid]"; ?>">
-<input type="submit" name="speichern" value="Speichern">
 </form>
-</center>
-
+<?
+} //nur anzeigen wenn bid gesetzt ist.
+?>
 
 
 
