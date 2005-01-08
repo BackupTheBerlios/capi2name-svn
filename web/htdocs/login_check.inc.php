e3b597a34e831dff24ae130b7d0f9ac13f45ab08
jonasge <jonasge@3730feeb-cfd4-0310-94ac-c51b6e4abe79>
2005-01-08 16:16:57 +0000

    lastlogin_*

--- web/htdocs/login_check.inc.php
+++ web/htdocs/login_check.inc.php
@@ -98,11 +98,11 @@ $result_userlist=$zugriff_mysql->sql_abfrage("SELECT * FROM userliste WHERE user
       }
       //update lastlogin_d and lastlogin_t
       //UPDATE capi_version SET version='0.6.7.2' WHERE id='1'
-      $datum=$zugriff_mysql->sql_abfrage("SELECT lastlogin_d FROM userliste WHERE username='$username'");
-      if (date("Y-m-d")!= $datum)
+      $datum=mysql_fetch_array($zugriff_mysql->sql_abfrage("SELECT lastlogin_d FROM userliste WHERE username='$username'"));
+     if ($datum[lastlogin_d]!=date("Y-m-d"))
        {
-      $zugriff_mysql->sql_abfrage("UPDATE userliste SET login_t=NOW() WHERE username='$username'");
-      $zugriff_mysql->sql_abfrage("UPDATE userliste SET login_d=NOW() WHERE username='$username'");
+      $zugriff_mysql->sql_abfrage("UPDATE userliste SET lastlogin_t=NOW() WHERE username='$username'");
+      $zugriff_mysql->sql_abfrage("UPDATE userliste SET lastlogin_d=NOW() WHERE username='$username'");
        }
     }//if passwd OK
    else
@@ -118,71 +118,9 @@ $result_userlist=$zugriff_mysql->sql_abfrage("SELECT * FROM userliste WHERE user
 $zugriff_mysql->close_mysql();
 
 
-/*
-// $login_name aus cookie und login_passwd aus cookie auslesen als md5 verschluesselt.
-$login_name="";
-$login_passwd="";
-$login_name =$_COOKIE['ck_username'];
-$login_passwd =$_COOKIE['ck_passwd'];
-$loginok="0";
-$zugriff_mysql->connect_mysql($sql["host"],$sql["dbuser"],$sql["dbpasswd"],$sql["db"] );
-$userliste=$zugriff_mysql->sql_abfrage("SELECT * FROM userliste");
-$nummer=mysql_numrows($userliste);
-for ($i = 0; $i < $nummer;$i++)
- {
-  $username = mysql_result($userliste, $i, "username");
-  $username = md5($username);
-   if ($username == $login_name)
-    {
-   //  print("check: Username OK");
-     $passwd = mysql_result($userliste, $i, "passwd");
-     $passwd = md5($passwd);
-     if ($passwd == $login_passwd)
-      {
-   //   print ("check: passwd OK");
-      $loginok="1";
-      if ($login_name="admin") { $loginok=1; }
-      $id = mysql_result($userliste, $i, "id");
-      $zeigteintrage=mysql_result($userliste, $i, "anzahl");
-      $msns=mysql_result($userliste, $i, "msns");
-      $wert=mysql_result($userliste, $i, "showrueckruf");
-       if ($wert=="checked") { $show_rueckruf="yes"; }
-       else { $show_rueckruf="no"; }
-
-      $wert=mysql_result($userliste, $i, "shownotiz");
-       if ($wert=="checked") { $shownotiz="yes"; }
-       else { $shownotiz="no"; }
-
-      $wert=mysql_result($userliste, $i, "showvorwahl");
-       if ($wert=="checked") { $show_vorwahl="yes"; }
-       else { $show_vorwahl="no"; }
-
-     $wert=mysql_result($userliste, $i, "showmsn");
-      if ($wert=="checked") { $show_msn="yes"; }
-      else { $show_msn="no"; }
-
-     $wert=mysql_result($userliste, $i, "showconfig");
-      if ($wert=="checked") { $showconfig="yes"; }
-      else { $showconfig="no"; }
-
-     $wert=mysql_result($userliste, $i, "showtyp");
-      if ($wert=="checked") { $showtyp="yes"; }
-      else { $showtyp="no"; }
-
-      $wert=mysql_result($userliste, $i, "loeschen");
-      if ($wert=="checked") { $showloeschen="yes"; }
-      else { $showloeschen="no"; }
-      }
-
-    }
-
- }
-$zugriff_mysql->close_mysql();
-
-*/
 
 
-//printf("ENDE: CHECK: Loginok: $loginok");
+
 if ($login_ok == 0)
  {
  include("./header.inc.php");
