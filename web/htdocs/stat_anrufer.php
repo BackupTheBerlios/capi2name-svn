afb7cab19e98ad1b730e2de50c54387a79af820a
jonasge <jonasge@3730feeb-cfd4-0310-94ac-c51b6e4abe79>
2005-03-13 21:17:00 +0000

    template

--- web/htdocs/stat_anrufer.php
+++ web/htdocs/stat_anrufer.php
@@ -1,6 +1,6 @@
 <?
 /*
-    copyright            : (C) 2002-2004 by Jonas Genannt
+    copyright            : (C) 2002-2005 by Jonas Genannt
     email                : jonasge@gmx.net
  ***************************************************************************/
 
@@ -17,27 +17,24 @@
 $seite=base64_encode("stat_anrufer.php");
 include("./login_check.inc.php");
 include("./header.inc.php");
-?>
+ 
+
+$zugriff_mysql->connect_mysql($sql["host"],$sql["dbuser"],$sql["dbpasswd"],$sql["db"] );
+$sql_query=sprintf("SELECT * FROM adressbuch WHERE id=%s", mysql_real_escape_string($_GET[id]));
+$result_adressbuch=$zugriff_mysql->sql_abfrage($sql_query);
+$data_adressbuch=mysql_fetch_array($result_adressbuch);
+$zugriff_mysql->close_mysql();
 
 
-
-<?
- $zugriff_mysql->connect_mysql($sql["host"],$sql["dbuser"],$sql["dbpasswd"],$sql["db"] );
- $result_adressbuch=$zugriff_mysql->sql_abfrage("SELECT * FROM adressbuch WHERE id=$_GET[id]");
- $data_adressbuch=mysql_fetch_array($result_adressbuch);
- $zugriff_mysql->close_mysql();
-
-echo "<div class=\"ueberschrift_seite\">$textdata[stat_anrufer_ueberschrift] $data_adressbuch[vorname] $data_adressbuch[nachname]</div>";
-echo "<br />";
-?>
+$template->set_filenames(array('overall_body' => 'templates/blueingrey/stat_anrufer.tpl'));
+$template->assign_vars(array(
+		'L_SITE_TITLE'  => $textdata[stat_anrufer_ueberschrift]." ".$data_adressbuch[vorname]." ".$data_adressbuch[nachname]));
 
 
-<?
 //Daten sammeln:
-
 $zugriff_mysql->connect_mysql($sql["host"],$sql["dbuser"],$sql["dbpasswd"],$sql["db"] ); $result_anrufe=$zugriff_mysql->sql_abfrage("SELECT * FROM angerufene WHERE rufnummer='$data_adressbuch[tele1]' OR rufnummer='$data_adressbuch[tele2]' OR rufnummer='$data_adressbuch[tele3]' OR rufnummer='$data_adressbuch[handy]'");
 $zugriff_mysql->close_mysql();
- $ges_anzahl=mysql_num_rows($result_anrufe);
+$ges_anzahl=mysql_num_rows($result_anrufe);
 
 
  if ($ges_anzahl!=false) //wenn gar kein anruf gibt
@@ -61,156 +58,98 @@ $wochen_genau=ceil($wochen);
  if ($wochen_genau==0) $wochen_genau=1;
 $anrufe_woche= round($ges_anzahl/$wochen_genau, 2);
 //ende daten sammeln!!!!
-?>
+
+
+$template->assign_block_vars('tab1',array(
+	'L_DETAIL_VIEW' => 'Detailansicht',
+	'L_ADDR_FRIST_NAME' => $textdata[addadress_vorname],
+	'L_ADDR_LAST_NAME' => $textdata[addadress_nachname],
+	'DATA_FIRST_NAME' => $data_adressbuch[vorname],
+	'DATA_LAST_NAME' => $data_adressbuch[nachname]));
 
 
 
-<table border="0" width="100%" style="margin-right:auto;margin-left:auto;text-align:left;">
-  <tr>
-    <td valign="top" >
-    <h3>Detailansicht</h3>
-      <table border="0" cellpadding="1" cellspacing="2">
-       <tr>
-             <td><? echo "$textdata[addadress_vorname]:"; ?></td>
-	     <td style="width:10px"> </td>
-	     <td><? echo "$data_adressbuch[vorname]"; ?></td>
-        </tr>
-	<tr>
-	   <td><? echo "$textdata[addadress_nachname]:"; ?></td>
-	   <td style="width:10px"> </td>
-	   <td><? echo "$data_adressbuch[nachname]"; ?></td>
-        </tr>
-<?
+
 if ($data_adressbuch[tele1]!="99")
- { echo "
-	<tr>
-	   <td>$textdata[addadress_telefonnummer1]:</td>
-	   <td style=\"width:10px\"> </td>
-	   <td>$data_adressbuch[tele1]</td>
-        </tr>";
-}
+ { 
+   $template->assign_block_vars('show_tele1',array(
+   		'L_TELE_1' => $textdata[addadress_telefonnummer1],
+		'DATA_TELE_1' => $data_adressbuch[tele1]));
+ }
 if ($data_adressbuch[tele2]!="99")
- { echo "
-
-	<tr>
-	   <td>$textdata[addadress_telefonnummer2]:</td>
-	   <td style=\"width:10px\"> </td>
-	   <td>$data_adressbuch[tele2]</td>
-        </tr>";
-}
+ {
+   $template->assign_block_vars('show_tele2',array(
+   		'L_TELE_2' => $textdata[addadress_telefonnummer2],
+		'DATA_TELE_2' => $data_adressbuch[tele2]));
+ }
 if ($data_adressbuch[tele3]!="99")
- { echo "
-	<tr>
-	   <td>$textdata[addadress_telefonnummer3]:</td>
-	   <td style=\"width:10px\"> </td>
-	   <td>$data_adressbuch[tele3]</td>
-        </tr>";
-}
+ {
+  $template->assign_block_vars('show_tele3',array(
+   		'L_TELE_3' => $textdata[addadress_telefonnummer3],
+		'DATA_TELE_3' => $data_adressbuch[tele3]));
+ }
 if ($data_adressbuch[handy]!="99")
- { echo "
-	<tr>
-	   <td>$textdata[addadress_handy]:</td>
-	   <td style=\"width:10px\"> </td>
-	   <td>$data_adressbuch[handy]</td>
-        </tr>";
-}
-echo "
-       </table>
-     <br />
-    <h3>$textdata[stat_anrufer_zahlen_fakten]</h3>
- <table border=\"0\">
-   <tr>
-    <td>$textdata[stat_anrufer_gesamte_anrufe]:</td>
-	<td>&nbsp;</td>
-	<td>$ges_anzahl</td>
-   </tr>
-   <tr>
-     <td>$textdata[stat_anrufer_geht_ueber]:</td>
-     <td>&nbsp;</td>
-     <td>$wochen_genau Wochen</td>
-   </tr>
-   <tr>
-    <td>$textdata[stat_anrufer_letzter_anruf]:</td>
-	<td>&nbsp;</td>
-	<td>$datum_letzter / $uhrzeit_letzer</td>
-   </tr>
-   <tr>
-    <td>$textdata[stat_anrufer_erster_anruf]:</td>
-	<td>&nbsp;</td>
-	<td>$datum_erster / $uhrzeit_erster</td>
-   </tr>
-   <tr>
-    <td>$textdata[stat_anrufer_durchschnitt_anrufe]:</td>
-	<td>&nbsp;</td>
-	<td>$anrufe_woche</td>
-   </tr>
-  </table>";
-  
-echo "<br/><img src=\"./stat-user.png.php?id=$_GET[id]\" style=\"border-width:0px;\"/>";
- echo "
-    </td>
-    <td style=\"vertical-align:top;\">
- <h3>$textdata[stat_anrufer_alle_anrufe_von] $data_adressbuch[vorname] $data_adressbuch[nachname]</h3>
-<table border=\"0\" cellpadding=\"5\" cellspacing=\"2\">
-  <tr>
-   <td style=\"text-align:center;\">$textdata[stat_anrufer_datum]</td>
-   <td style=\"text-align:center;\">$textdata[stat_anrufer_uhrzeit]</td>
-   <td style=\"text-align:center;\">$textdata[stat_anrufer_rufnummer]</td>
-   <td style=\"text-align:center;\">$textdata[stat_anrufer_MSN]</td>
-  </tr>";
+ { 
+  $template->assign_block_vars('show_cell_phone',array(
+		'L_CELL_PHONE' => $textdata[addadress_handy],
+		'DATA_CELL_PHONE' => $data_adressbuch[handy]));
+ }
+ 
+ 
+$template->assign_block_vars('tab2',array(
+	'L_STAT_CALLERS_COUNTS' => $textdata[stat_anrufer_zahlen_fakten],
+	'L_ALL_CALLS' => $textdata[stat_anrufer_gesamte_anrufe],
+	'DATA_ALL_CALLS' => $ges_anzahl,
+	'L_STAT_TIME' => $textdata[stat_anrufer_geht_ueber],
+	'DATA_WEEKS' => $wochen_genau,
+	'L_WEKKS' => 'Wochen',
+	'L_LAST_CALL' => $textdata[stat_anrufer_letzter_anruf],
+	'DATA_LAST_DATE' => $datum_letzter,
+	'DATA_LAST_TIME' => $uhrzeit_letzer,
+	'L_FRIST_CALL' => $textdata[stat_anrufer_erster_anruf],
+	'DATA_FIRST_DATE' => $datum_erster,
+	'DATA_FIRST_TIME' => $uhrzeit_erster,
+	'L_CALLS_AVERAGE' => $textdata[stat_anrufer_durchschnitt_anrufe],
+	'DATA_CALLS_AVERAGE' => $anrufe_woche)); 
+ 
+$template->assign_vars(array('DATA_ID_CALLERS' => $_GET[id])); 
 
-  $i=0;
+$template->assign_block_vars('tab3',array(
+	'L_LSIT_ALL_CALLS' => $textdata[stat_anrufer_alle_anrufe_von]." ".$data_adressbuch[vorname]." ".$data_adressbuch[nachname],
+	'L_DATE' => $textdata[stat_anrufer_datum],
+	'L_TIME' => $textdata[stat_anrufer_uhrzeit],
+	'L_CALL_NUMBER' => $textdata[stat_anrufer_rufnummer],
+	'L_CALL_TO_MSN' => $textdata[stat_anrufer_MSN]));
+$i=0;
+
 $zugriff_mysql->connect_mysql($sql["host"],$sql["dbuser"],$sql["dbpasswd"],$sql["db"] );
-$result=$zugriff_mysql->sql_abfrage("SELECT * FROM angerufene WHERE rufnummer='$data_adressbuch[tele1]' OR rufnummer='$data_adressbuch[tele2]' OR rufnummer='$data_adressbuch[tele3]' OR rufnummer='$data_adressbuch[handy]' ORDER BY id DESC");
+$result=$zugriff_mysql->sql_abfrage("SELECT id,datum,uhrzeit,msn,rufnummer FROM angerufene WHERE rufnummer='$data_adressbuch[tele1]' OR rufnummer='$data_adressbuch[tele2]' OR rufnummer='$data_adressbuch[tele3]' OR rufnummer='$data_adressbuch[handy]' ORDER BY id DESC");
 $zugriff_mysql->close_mysql();
  while ($data_angerufene=mysql_fetch_array($result))
   {
   if($i%2==0)
-   { $color=$c_color[11]; }
+   { $color=$row_color_1; }
     else
-    { $color=$c_color[12]; }
-   $tmp=mysql_datum($data_angerufene[datum]);
-   echo "
-   <tr style=\"background-color:$color\">
-    <td>$tmp</td>
-	<td style=\"text-align:center;\">$data_angerufene[uhrzeit]</td>
-	<td style=\"text-align:center;\">$data_angerufene[rufnummer]</td>
-	<td style=\"text-align:center;\">$data_angerufene[msn]</td>
-    </tr>";
+    { $color=$row_color_2; }
+
+   $template->assign_block_vars('tab4', array(
+   	'DATA_COLOR' => $color,
+	'DATA_DATE' => mysql_datum($data_angerufene[datum]),
+	'DATA_TIME' => $data_angerufene[uhrzeit],
+	'DATA_NUMBER' => $data_angerufene[rufnummer],
+	'DATA_CALL_TO_MSN' => $data_angerufene[msn]));
    $i++;
   }
  
 
-   } // gar kein anruf da ist:
-   else
-    {
-     echo "<br /><span style=\"text-align:center\">$textdata[stat_anrufer_keine_anrufe_gefunden] $data_adressbuch[vorname] $data_adressbuch[nachname] $textdata[stat_anrufer_keine_anrufe_gefunden_ende]</span>";
-    }
+ } // gar kein anruf da ist:
+else
+ {
+  $template->assign_block_vars('no_call_from_user',array(
+  	'L_MSG_NO_CALL' => $textdata[stat_anrufer_keine_anrufe_gefunden]." ".$data_adressbuch[vorname]." ".$data_adressbuch[nachname]." ".$textdata[stat_anrufer_keine_anrufe_gefunden_ende]));
+ }
+
+
+$template->pparse('overall_body');
+include("./footer.inc.php");
 ?>
-
-
- </table>
-
-    </td>
-  </tr>
-</table>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  <?
-  include("./footer.inc.php");
-  ?>
---
+++ web/htdocs/templates/blueingrey/stat_anrufer.tpl
@@ -0,0 +1,116 @@
+<div class="ueberschrift_seite">{L_SITE_TITLE}</div>
+<br />
+
+<!-- BEGIN tab1 -->
+<table border="0" width="100%" style="margin-right:auto;margin-left:auto;text-align:left;">
+ <tr>
+ <td valign="top" >
+ <h3>{tab1.L_DETAIL_VIEW}</h3>
+      <table border="0" cellpadding="1" cellspacing="2">
+       <tr>
+             <td>{tab1.L_ADDR_FRIST_NAME}:</td>
+	     <td style="width:10px"> </td>
+	     <td>{tab1.DATA_FIRST_NAME}</td>
+        </tr>
+	<tr>
+	   <td>{tab1.L_ADDR_LAST_NAME}:</td>
+	   <td style="width:10px"> </td>
+	   <td>{tab1.DATA_LAST_NAME}</td>
+        </tr>
+<!-- END tab1 -->
+<!-- BEGIN show_tele1 -->
+<tr>
+   <td>{show_tele1.L_TELE_1}:</td>
+   <td style="width:10px"> </td>
+   <td>{show_tele1.DATA_TELE_1}</td>
+</tr>
+<!-- END show_tele1 -->
+<!-- BEGIN show_tele2 -->
+<tr>
+   <td>{show_tele2.L_TELE_2}:</td>
+   <td style="width:10px"> </td>
+   <td>{show_tele2.DATA_TELE_2}</td>
+</tr>
+<!-- END show_tele2 -->
+<!-- BEGIN show_tele3 -->
+<tr>
+   <td>{show_tele3.L_TELE_3}:</td>
+   <td style="width:10px"> </td>
+   <td>{show_tele3.DATA_TELE_3}</td>
+</tr>
+<!-- END show_tele3 -->
+<!-- BEGIN show_cell_phone -->
+<tr>
+   <td>{show_cell_phone.L_CELL_PHONE}:</td>
+   <td style="width:10px"> </td>
+   <td>{show_cell_phone.DATA_CELL_PHONE}</td>
+</tr>
+<!-- END show_tele3 -->
+</table>
+<br />
+
+<!-- BEGIN tab2 -->
+<h3>{tab2.L_STAT_CALLERS_COUNTS}</h3>
+ <table border="0">
+  <tr>
+    <td>{tab2.L_ALL_CALLS}:</td>
+    <td>&nbsp;</td>
+    <td>{tab2.DATA_ALL_CALLS}</td>
+  </tr>
+  <tr>
+    <td>{tab2.L_STAT_TIME}:</td>
+    <td>&nbsp;</td>
+    <td>{tab2.DATA_WEEKS} {tab2.L_WEKKS}</td>
+  </tr>
+  <tr>
+    <td>{tab2.L_LAST_CALL}:</td>
+    <td>&nbsp;</td>
+    <td>{tab2.DATA_LAST_DATE} / {tab2.DATA_LAST_TIME}</td>
+  </tr>
+  <tr>
+    <td>{tab2.L_FRIST_CALL}:</td>
+    <td>&nbsp;</td>
+    <td>{tab2.DATA_FIRST_DATE} / {tab2.DATA_FIRST_TIME}</td>
+  </tr>
+  <tr>
+    <td>{tab2.L_CALLS_AVERAGE}:</td>
+    <td>&nbsp;</td>
+    <td>{tab2.DATA_CALLS_AVERAGE}</td>
+  </tr>
+</table>
+<!-- END tab2 -->
+<br/>
+<img src="./stat-user.png.php?id={DATA_ID_CALLERS}" style="border-width:0px;"/>
+
+<!-- BEGIN tab3 -->
+</td>
+  <td style="vertical-align:top;">
+  <h3>{tab3.L_LSIT_ALL_CALLS}</h3>
+
+<table border="0" cellpadding="5" cellspacing="2">
+  <tr>
+   <td style="text-align:center;">{tab3.L_DATE}</td>
+   <td style="text-align:center;">{tab3.L_TIME}</td>
+   <td style="text-align:center;">{tab3.L_CALL_NUMBER}</td>
+   <td style="text-align:center;">{tab3.L_CALL_TO_MSN}</td>
+  </tr>
+<!-- END tab3 -->
+
+<!-- BEGIN tab4 -->
+<tr style="background-color:{tab4.DATA_COLOR}">
+<td>{tab4.DATA_DATE}</td>
+<td style="text-align:center;">{tab4.DATA_TIME}</td>
+<td style="text-align:center;">{tab4.DATA_NUMBER}</td>
+<td style="text-align:center;">{tab4.DATA_CALL_TO_MSN}</td>
+</tr>
+<!-- END tab4 -->
+</table>
+</td>
+</tr>
+</table>
+
+<!-- BEGIN no_call_from_user -->
+<br />
+<span style="text-align:center">{no_call_from_user.L_MSG_NO_CALL}</span>
+<!-- END no_call_from_user -->
+
