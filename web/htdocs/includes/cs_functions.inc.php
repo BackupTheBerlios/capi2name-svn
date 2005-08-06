667553c4ae89acf35451e022695a1854747e8b8a
jonasge <jonasge@3730feeb-cfd4-0310-94ac-c51b6e4abe79>
2005-08-06 13:06:04 +0000

    bugfix

--- web/htdocs/includes/cs_functions.inc.php
+++ web/htdocs/includes/cs_functions.inc.php
@@ -13,26 +13,7 @@
  *                                                                         *
  ***************************************************************************/
   // 	editor: Kai Römer 
-function nummer2Name($nummer) 
-{
-	$tab_adressbuch = mysql_query("SELECT t1.name_first,t1.name_last,t1.id,
-	 	t2.areacode
-		FROM phonenumbers AS t2 
-		LEFT JOIN addressbook AS t1 ON t2.addr_id=t1.id WHERE t2.number='$nummer'");
-		if (mysql_num_rows($tab_adressbuch) == 0)
-		{
-			return "<a href=\"addressbook_add.php?rufnr=$nummer\">$nummer</a>";
-		}
-		else if (mysql_num_rows($tab_adressbuch) == 1) {
-			$data=mysql_fetch_assoc($tab_adressbuch);
-			if ($fax == $nummer) $zusatz = "(Fax)";
-			if ($handy == $nummer) $zusatz = "(Handy)";
-			return "<a href=\"addressbook.php?id=$data[id]#find\">$data[name_first] $data[name_last]</a>";
-		}
-		
-		return "error";
-	}
-	
+
 	function cmp($a, $b) {
 		if (preg_replace("/(.*;)(.*)/","\\1", $a) == preg_replace("/(.*;)(.*)/","\\1", $b)) {
 			return 0;
@@ -45,12 +26,5 @@ function nummer2Name($nummer)
 		return $retval;
 	}
 
-$dataB->sql_connect($sql["host"],$sql["dbuser"],$sql["dbpasswd"],$sql["db"] );
 
-$result=$dataB->sql_query("SELECT * FROM config WHERE conf LIKE 'cs_%'");
- while($daten_cs=$dataB->sql_fetch_assoc($result))
-  {
-   $cs_conf[$daten_cs[conf]]=$daten_cs[value];
-  }
-$dataB->sql_close();
 ?>
